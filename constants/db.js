d6119114976fd419aba6d0776856a69c511d91bd
MacBook Air <devkevinrivera@gmail.com>
2023-01-13 13:30:09 +0100

    .

---
+++ constants/db.js
@@ -0,0 +1,62 @@
+export const DB = {
+    musclesRoutines: [
+        {
+            name: 'Espalda',
+            listExercises: [
+                {
+                    nombre: 'Dominadas agarre neutro y prono',
+                    descripcion: '2 repeticiones de cada agarre intercaladas entre ellas el número máximo de repeticiones que puedas'
+                },
+                {
+                    nombre: 'Jalon el pecho barra ancha',
+                    descripcion: '4 series - x15 - x12 - x10 - x10 repeticiones en cada serie se a de añadir más peso que en la anterior'
+                },
+                {
+                    nombre: 'Remo con barra', descripcion: '2 series de 12 repeticiones 2 series de 10 repeticiones'
+                },
+            ]
+        },
+        {
+            name: 'Biceps',
+            listExercises: [
+                {
+                    nombre: 'Biceps barra z',
+                    descripcion: '4 series x 12 repeticiones'
+                },
+                {
+                    nombre: 'Biceps martillo',
+                    descripcion: '4 series x 12 repeticiones'
+                },
+                {
+                    nombre: 'Remo con barra', 
+                    descripcion: '4 series x 12 repeticiones'
+                },
+            ]
+        },
+        {
+            name: 'Hombros',
+            listExercises: [
+                {
+                    nombre: 'Hombro posterior en maquina',
+                    descripcion: '4 series de 14 repeticiones'
+                },
+                {
+                    nombre: 'Trapesio + serrato',
+                    descripcion: '4 series x 12 repeticiones'
+                },
+                {
+                    nombre: 'Suspension en barra', 
+                    descripcion: '5 series aguantando lo maximo posible la bajada'
+                },
+                {
+                    nombre: 'L Sit', 
+                    descripcion: '4 series de lo que puedas, si no puedes levantar las piernas levanta las rodillas'
+                },
+                {
+                    nombre: 'Plancha Normal', 
+                    descripcion: 'Aguantar el maximo tiempo posible'
+                },
+            ]
+        },
+    ]
+}
